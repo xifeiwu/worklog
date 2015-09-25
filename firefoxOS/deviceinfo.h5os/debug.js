aa0dcf34e3ecda2cd13099ca970f5b4bfe10794b
xifei.wu <xifei.wu@acadine.com>
2015-09-25 19:16:21 +0800

    fix some files

---
+++ firefoxOS/deviceinfo.h5os/debug.js
@@ -0,0 +1,21 @@
+# Get A particular Module.
+var module = "softkey_handler";
+
+function getModule(name){
+var module = require(name);
+    console.log(module);
+}
+getModule(module);
+//var SoftkeyHandler = require('softkey_handler');
+
+
+
+# Open a Panel
+var SettingsService = require('modules/settings_service');
+SettingsService.navigate("about");
+root
+
+document.querySelector("section.current");
+document.querySelector("section#about");
+
+window.LaunchContext
