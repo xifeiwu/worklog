03804cb06fb5016bade5ca298d4a1ff0975ec2e2
xifei.wu <xifei.wu@acadine.com>
2015-10-08 15:00:31 +0800

    ftu_ping v0.2

--- h5os/gaia/ftu_ping.js
+++ h5os/gaia/ftu_ping.js
@@ -29,8 +29,35 @@
   const URL_SETTINGS = ['deviceinfo.platform_build_id',
                         'deviceinfo.platform_version',
                         'app.update.channel'];
+1.Abstract
+Where are related files?
+Related parameters is accquired in the file apps/system/js/ftu_ping.js, the start point function is ensurePing().
+window.TelemetryRequest is exported in file shared/telemetry/telemetry.js, function TelemetryRequest.getDeviceID() is used in ftu_ping.
 
+How to get parameters in WebIDL?
+var ftuping = new FtuPing();
+ftuping.ensurePing();
+some parameters is get through function tryPing()
+ftuping.tryPing();
+ftuping.assemblePingData();
 
+2.Related Code
+var _pingData = {}
+2.1
+const FTU_PING_ID = 'ftu.pingID';
+  var promise = TelemetryRequest.getDeviceID(FTU_PING_ID);
+  promise.then(function(deviceID) {
+    //self.debug('Found deviceID: ' + deviceID);
+    console.log('Found deviceID: ' + deviceID);
+    _pingData.pingID = deviceID;
+    // resolve();
+  }).catch(function(error) {
+    console.log('Generating deviceID: ' + error);
+    _pingData.pingID = uuid();
+    window.asyncStorage.setItem(FTU_PING_ID, _pingData.pingID);
+    // resolve();
+  });
+2.2
 const FTU_PING_URL = 'ftu.pingURL';
   asyncStorage.getItem('ftu.pingID', function(value) {
     console.log('The value of key is:', value);
@@ -44,7 +71,7 @@ const FTU_PING_URL = 'ftu.pingURL';
     console.log(imei);
   }).catch(function(){console.log("Error");});
 
-
+2.3
 const FTU_PING_ACTIVATION = 'ftu.pingActivation';
 const FTU_PING_ENABLED = 'ftu.pingEnabled';
 const FTU_PING_NETWORK_FAIL_COUNT = 'ftu.pingNetworkFailCount';
@@ -74,7 +101,7 @@ const FTU_PING_NETWORK_FAIL_COUNT = 'ftu.pingNetworkFailCount';
     console.log("_networkFailCount: " + _networkFailCount);
 	});
 
-
+2.4
 // Settings to observe value changes for while the ping has not been sent
 const OBSERVE_SETTINGS = ['deviceinfo.os',
                           'deviceinfo.software',
@@ -137,6 +164,7 @@ const DEFAULT_MAX_NETWORK_FAILS = 24;
     app.update.channel: default
   */
 
+2.5
 self._pingData.screenHeight = window.screen.height;
 self._pingData.screenWidth = window.screen.width;
 self._pingData.devicePixelRatio = window.devicePixelRatio;
@@ -150,6 +178,7 @@ const DEFAULT_PING_TIMEOUT = 60 * 1000;
 const TELEMETRY_VERSION = 1;
 const TELEMETRY_REASON = 'ftu';
 
+2.6
 this._pingData.preinstalled
   function initPreinstalledApps(){
     window.navigator.mozApps.mgmt.getAll().onsuccess = function(evt) {
@@ -170,3 +199,39 @@ this._pingData.preinstalled
           });
     };
   }
+
+2.7
+network and icc are append to _pingData in function checkMobileNetwork, which is called in function tryPing().
+  _pingData.network = [];
+  _pingData.icc = [];
+
+  function getNetworkAndICC(){
+    if (SIMSlotManager.noSIMCardConnectedToNetwork()) {
+      console.log('No SIM cards connected to a network');
+    }
+    var conns = window.navigator.mozMobileConnections;
+    if (!conns || conns.length === 0) {
+      console.log('No mobile connections');
+      return;
+    }
+
+    var slots = SIMSlotManager.getSlots().filter(function(slot) {
+      return !slot.isAbsent() && !slot.isLocked();
+    });
+
+    if (slots.length === 0) {
+      console.log('No unlocked or active SIM cards found');
+      return;
+    }
+  }
+
+2.8 not found place where deviceinfo.last_updated is set
+apps/settings/build_stage/settings/js/panels/about/panel.js:      var key = 'deviceinfo.last_updated';
+apps/settings/js/panels/about/update_check.js:      var key = 'deviceinfo.last_updated';
+apps/system/js/ftu_ping.js:                            'deviceinfo.last_updated',
+.repo/project-objects/system.git/rr-cache/2cf5d1eb755bcb7fcacee74ebf725ea72507a2c6/preimage:                            'deviceinfo.last_updated',
+
+  etImei().then(function (imeis) {
+            console.log('imeis : ' + imeis);
+            self._pingData['deviceinfo.MEID'] = imeis;
+          });
