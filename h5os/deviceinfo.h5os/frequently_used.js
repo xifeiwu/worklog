b3c4b9a635bcd999294d4a9bf789f175f6604b81
xifei.wu <xifei.wu@acadine.com>
2015-11-04 11:26:55 +0800

    remove h5os to other repo.

--- h5os/deviceinfo.h5os/frequently_used.js
+++
@@ -1,8 +0,0 @@
-var module = "softkey_handler";
-
-function getModule(name){
-var module = require(name);
-    console.log(module);
-}
-getModule(module);
-//var SoftkeyHandler = require('softkey_handler');
